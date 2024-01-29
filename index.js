aa14d4ca55e2aa33c64dfee9bbf066c02abb0018
Haley-Crawford <156232728+Haley-Crawford@users.noreply.github.com>
2024-01-28 19:28:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//Hide rules panel
+const hideRules = document.getElementById('close-rules');
+const div = document.getElementById("rules");
+
+hideRules.addEventListener('click', () => {
+    div.style.display = 'none';
+})
